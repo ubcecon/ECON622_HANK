6a32af0248867bcb074da55a4956922172518e25
Paul Schrimpf <paul.schrimpf@gmail.com>
2022-10-04 12:17:14 -0700

    Added testing setup.

---
+++ src/HANK.jl
@@ -0,0 +1,9 @@
+module HANK
+
+export totest
+
+function totest(x)
+    return(sum(x))
+end
+
+end
---
+++ test/example.jl
@@ -0,0 +1,8 @@
+@testset "Example tests" begin
+    for x ∈ [1, 0.0, rand(), 1//4]
+        @test totest(x) ≈ x
+    end
+    for x ∈ [rand(10), rand(2,3), (1,3)]
+        @test totest(x) ≈ sum(x)
+    end
+end
